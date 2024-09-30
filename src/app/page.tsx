f18c484df79c3cc1b984e067ee07871f9a39765c
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-09-30 15:47:34 +0530

    setup redux toolkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
-
 export default function Home() {
   return (
     <div className="w-full min-h-dvh">
-      hello <Button onClick={() => signIn("google")}>Login</Button>
+      hello 
     </div>
   );
 }
